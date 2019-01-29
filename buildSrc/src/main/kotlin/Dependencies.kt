156adb960efaad95a8f6e2b7ba623fd3e55f94c2
Filipe Belatti <fibelatti@gmail.com>
2019-01-29 21:03:17 +0000

    Updates buildSrc Dependencies file to reduce complexity

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,90 +1,52 @@
 object Versions {
-    internal const val gradlePluginVersion = "3.2.1"
-    internal const val kotlinVersion = "1.3.11"
-
-    const val minSdkVersion = 19
+    const val minSdkVersion = 21
     const val targetSdkVersion = 28
     const val compileSdkVersion = 28
-
     const val buildToolsVersion = "28.0.3"
 
-    internal const val appCompatVersion = "1.0.2"
-    internal const val materialDesignVersion = "1.0.0"
-    internal const val supportAnnotationVersion = "1.0.0"
-
-    internal const val archComponentsVersion = "2.0.0"
-
-    internal const val coroutinesCoreVersion = "1.0.0"
-    internal const val coroutinesAndroidVersion = "1.0.0"
-
-    internal const val rxJavaVersion = "2.2.4"
-    internal const val rxAndroidVersion = "2.1.0"
-
-    internal const val javaxInjectVersion = "1"
-
-    internal const val moshiVersion = "1.8.0"
-
-    // region Distribution
-    internal const val bintrayPluginVersion = "1.8.0"
-    // endregion
-
-    // region Testing
-    internal const val jUnitVersion = "4.12"
-    internal const val testRunnerVersion = "1.1.0"
-    internal const val mockitoVersion = "2.23.0"
-    internal const val mockitoAndroidVersion = "2.18.3"
-    internal const val junit5Version = "5.3.1"
-    // endregion
+    internal const val kotlinVersion = "1.3.20"
+    internal const val coroutinesVersion = "1.1.1"
+    internal const val lifeCycleVersion = "2.0.0"
 }
 
 object Classpaths {
-    val gradlePlugin = "com.android.tools.build:gradle:${Versions.gradlePluginVersion}"
-    val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
-    val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:${Versions.bintrayPluginVersion}"
+    const val gradlePlugin = "com.android.tools.build:gradle:3.2.1"
+    const val kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val bintrayPlugin = "com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.0"
 }
 
-object KotlinDependencies {
-    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}"
-}
+object Dependencies {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersion}"
+    const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesVersion}"
+    const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesVersion}"
 
-object SupportLibraryDependencies {
-    val supportLibrary = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
-    val materialDesign = "com.google.android.material:material:${Versions.materialDesignVersion}"
-    val supportAnnotations = "androidx.annotation:annotation:${Versions.supportAnnotationVersion}"
-}
+    const val supportLibrary = "androidx.appcompat:appcompat:1.1.0-alpha01"
+    const val materialDesign = "com.google.android.material:material:1.1.0-alpha02"
+    const val supportAnnotations = "androidx.annotation:annotation:1.0.0"
 
-object ArchitectureComponentDependencies {
-    val archComponents = "androidx.lifecycle:lifecycle-extensions:${Versions.archComponentsVersion}"
-    val archComponentsCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.archComponentsVersion}"
-}
+    const val archComponents = "androidx.lifecycle:lifecycle-extensions:${Versions.lifeCycleVersion}"
+    const val archComponentsCompiler = "androidx.lifecycle:lifecycle-compiler:${Versions.lifeCycleVersion}"
 
-object CoroutineDependencies {
-    val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutinesCoreVersion}"
-    val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutinesAndroidVersion}"
-}
+    val rxJava = "io.reactivex.rxjava2:rxjava:2.2.4"
+    val rxAndroid = "io.reactivex.rxjava2:rxandroid:2.1.0"
 
-object RxDependencies {
-    val rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJavaVersion}"
-    val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
-}
+    val javaxInject = "javax.inject:javax.inject:1"
 
-object DIDependencies {
-    val javaxInject = "javax.inject:javax.inject:${Versions.javaxInjectVersion}"
-}
-
-object ThirdPartyDependencies {
-    val moshiKotlin = "com.squareup.moshi:moshi-kotlin:${Versions.moshiVersion}"
+    const val moshiKotlin = "com.squareup.moshi:moshi-kotlin:1.8.0"
 }
 
 object TestDependencies {
-    val junit = "junit:junit:${Versions.jUnitVersion}"
-    val junit5 = "org.junit.jupiter:junit-jupiter-api:${Versions.junit5Version}"
-    val junit5Engine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junit5Version}"
-    val junit5Params = "org.junit.jupiter:junit-jupiter-params:${Versions.junit5Version}"
-    val junitVintage = "org.junit.vintage:junit-vintage-engine:${Versions.junit5Version}"
-    val testRunner = "com.android.support.test:runner:${Versions.testRunnerVersion}"
-    val kotlinTest = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
-    val mockitoCore = "org.mockito:mockito-core:${Versions.mockitoVersion}"
-    val mockitoAndroid = "org.mockito:mockito-android:${Versions.mockitoAndroidVersion}"
-    val archComponentsTest = "android.arch.core:core-testing:${Versions.archComponentsVersion}"
+    private const val junit5Version = "5.3.1"
+
+    const val junit = "junit:junit:4.12"
+    const val junit5 = "org.junit.jupiter:junit-jupiter-api:$junit5Version"
+    const val junit5Engine = "org.junit.jupiter:junit-jupiter-engine:$junit5Version"
+    const val junit5Params = "org.junit.jupiter:junit-jupiter-params:$junit5Version"
+    const val junitVintage = "org.junit.vintage:junit-vintage-engine:$junit5Version"
+    const val testRunner = "com.android.support.test:runner:1.1.0"
+    const val kotlinTest = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersion}"
+    const val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutinesVersion}"
+    const val mockitoCore = "org.mockito:mockito-core:2.23.0"
+    const val mockitoAndroid = "org.mockito:mockito-android:2.18.3"
+    const val archComponentsTest = "android.arch.core:core-testing:${Versions.lifeCycleVersion}"
 }
