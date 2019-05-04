88b0291564faa0b8bf9ab6aa8df834ae5a626f7f
Filipe Belatti <fibelatti@gmail.com>
2019-05-04 12:07:33 +0100

    Updates LibInfo to support alpha releases

--- buildSrc/src/main/kotlin/LibInfo.kt
+++ buildSrc/src/main/kotlin/LibInfo.kt
@@ -1,13 +1,24 @@
+@Suppress("MagicNumber")
 object LibInfo {
     private const val versionMajor = 1
-    private const val versionMinor = 0
+    private const val versionMinor = 1
     private const val versionPatch = 0
+    private const val versionAlpha = 1
 
-    private const val majorMultiplier = 10000
-    private const val minorMultiplier = 100
+    private const val majorMultiplier = 1000000
+    private const val minorMultiplier = 10000
+    private const val patchMultiplier = 100
 
-    val versionCode: Int = versionMajor * majorMultiplier + versionMinor * minorMultiplier + versionPatch
-    val versionName: String = "${LibInfo.versionMajor}.${LibInfo.versionMinor}.${LibInfo.versionPatch}"
+    val versionCode: Int = (
+        versionMajor * majorMultiplier +
+            versionMinor * minorMultiplier +
+            versionPatch * patchMultiplier +
+            (versionAlpha.takeIf { it != 0 } ?: 99)
+        ).also { println("versionCode is $it") }
+
+    val versionName: String =
+        ("$versionMajor.$versionMinor.$versionPatch${versionAlpha.takeIf { it != 0 }?.let { "-alpha$it" } ?: ""}")
+            .also { println("versionName is $it") }
 
     const val repo = "fibelatti-core"
     const val repoUrl = "https://github.com/fibelatti/fibelatti-core"
@@ -19,7 +30,8 @@ object LibInfo {
 
     const val libGroupId = "com.fibelatti.core"
 
-    const val libDescription = "Commonly used classes and extensions functions. Used to build Android projects in Kotlin."
+    const val libDescription =
+        "Commonly used classes and extensions functions. Used to build Android projects in Kotlin."
 
     const val devId = "fibelatti"
     const val devName = "Filipe Belatti"
