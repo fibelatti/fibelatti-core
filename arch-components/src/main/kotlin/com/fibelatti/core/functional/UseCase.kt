20f42f52196deef19d88d78051cb096a646ebb44
Filipe Belatti <fibelatti@gmail.com>
2019-05-19 19:21:35 +0100

    Adds UseCase classes to handle LiveData

---
+++ arch-components/src/main/kotlin/com/fibelatti/core/functional/UseCase.kt
@@ -0,0 +1,33 @@
+package com.fibelatti.core.functional
+
+import androidx.lifecycle.LiveData
+
+/**
+ * Handy class to define single responsibility use cases that return LiveData<Result<T>>. It can be invoked as a
+ * function thanks to operator overloading.
+ *
+ * class Foo : LiveDataUseCase<Bar>
+ *
+ * val result: LiveData<Result<Bar>> = foo()
+ */
+abstract class LiveDataUseCase<Type> where Type : Any {
+
+    abstract suspend fun run(): LiveData<Result<Type>>
+
+    suspend operator fun invoke(): LiveData<Result<Type>> = run()
+}
+
+/**
+ * Handy class to define single responsibility use cases that return LiveData<Result<T>> and require parameters. It can
+ * be invoked as a function thanks to operator overloading.
+ *
+ * class Foo : LiveDataUseCaseWithParams<Bar, Foo.Params>
+ *
+ * val result: LiveData<Result<Bar>> = foo(Foo.Params(baz))
+ */
+abstract class LiveDataUseCaseWithParams<Type, in Params> where Type : Any {
+
+    abstract suspend fun run(params: Params): LiveData<Result<Type>>
+
+    suspend operator fun invoke(params: Params): LiveData<Result<Type>> = run(params)
+}
